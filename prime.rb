af59defe7f956c915847c5643a63b7e8ba8cbfca
Salem Asrat <salemasrat@gmail.com>
2019-10-19 00:47:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_test| 
+      number % num_test == 0
+    end
+  else
+    false
+  end
+end
